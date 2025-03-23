2d9fc7cf96f39b945ff9a9ae1ffe62ebd13ca0aa
Muhammed Jimoh <Muhammedjimoh47@gmail.com>
2025-03-23 08:47:03 +0000

    feat: iaac setup

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = false
+}
+
+output "random_bucket_name" {
+    value = random_string.bucket_name.result
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+provider "random" {
+  # Configuration options
+}
+
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+resource "random_string" "bucket_name" {
+  upper            = false
+  length           = 32
+  special          = false
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  # Bucket Naming rules
+  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  bucket = random_string.bucket_name.result
+}
+
+output "random_bucket_name" {
+    value = random_string.bucket_name.result
+}
