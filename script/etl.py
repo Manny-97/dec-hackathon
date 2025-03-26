b7793fd31e72d5c2f312ed577d9ea1999d8f7e14
Choice Ugwuede <168081987+Choiceugwuede@users.noreply.github.com>
2025-03-26 12:08:10 +0100

    Update etl.py

--- script/etl.py
+++ script/etl.py
@@ -23,8 +23,16 @@ def read_data(page=0):
     API_KEY = os.environ.get('API_KEY', '')
     params = {
         "api_key": API_KEY,
-        "fields": "id,school.name,school.city,school.state,school.zip,school.ownership,school.school_url,school.price_calculator_url,school.men_only,school.women_only,latest.admissions.admission_rate.overall,latest.cost.tuition.in_state,latest.cost.tuition.out_of_state,latest.earnings.10_yrs_after_entry.median,latest.admissions.sat_scores.average.overall,latest.completion.completion_rate_4yr_150nt,latest.student.demographics.student_faculty_ratio,school.carnegie_basic,latest.student.demographics.race_ethnicity.non_resident_alien,latest.school.instructional_expenditure_per_fte,latest.school.tuition_revenue_per_fte,latest.school.endowment.end,latest.student.retention_rate.four_year.full_time_pooled,latest.aid.pell_grant_rate",
-        "per_page": 100,  
+        "fields": "id,school.name,school.city,school.state,school.zip,school.ownership,school.school_url,"
+                  "school.price_calculator_url,school.men_only,school.women_only,"
+                  "latest.admissions.admission_rate.overall,latest.cost.tuition.in_state,latest.cost.tuition.out_of_state,"
+                  "latest.earnings.10_yrs_after_entry.median,latest.admissions.sat_scores.average.overall,"
+                  "latest.completion.completion_rate_4yr_150nt,latest.student.demographics.student_faculty_ratio,"
+                  "school.carnegie_basic,latest.student.demographics.race_ethnicity.non_resident_alien,"
+                  "latest.school.instructional_expenditure_per_fte,latest.school.tuition_revenue_per_fte,"
+                  "latest.school.endowment.end,latest.student.retention_rate.four_year.full_time_pooled,"
+                  "latest.aid.pell_grant_rate,school,school.institutional_characteristics.level,school.carnegie_undergrad",
+        "per_page": 100,
         "page": page
     }
     # Endpoint
