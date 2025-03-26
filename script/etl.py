4cce22b8fbbf728679a8ea6be5cc324f661190a8
Choice Ugwuede <168081987+Choiceugwuede@users.noreply.github.com>
2025-03-26 12:03:09 +0100

    Update etl.py

--- script/etl.py
+++ script/etl.py
@@ -29,58 +29,101 @@ def read_data(page=0):
     }
     # Endpoint
     BASE_URL = "https://api.data.gov/ed/collegescorecard/v1/schools"
-    data_list = []
-    response = session.get(
-        url=BASE_URL, params=params, timeout=30
-        )
-    if response.status_code != 200:
-        print(f"Error {response.status_code}: {response.text}")  # Log the error message
-        return None
     try:
-        if response.text.strip():
-
-            results = response.json()['results']
-            ownership_mapping = {
-                1: "Public",
-                2: "Private Nonprofit",
-                3: "Private For-Profit"
-            }
-            research_mapping= {
-                15: 1,
-                16: 2,
-                27: 3,
-                18: 4,
-                19: 5,
-                20: 6"
-            }
+        response = session.get(url=BASE_URL, params=params, timeout=30)
+        response.raise_for_status()  # Raise HTTP errors
+        results = response.json().get('results', [])
+        if not results:
+            print(f"Warning: Empty response received on page {page}")
+            return None
+        ownership_mapping = {1: "Public", 2: "Private Nonprofit", 3: "Private For-Profit"}
+        research_mapping = {15: 1, 16: 2, 27: 3, 18: 4, 19: 5, 20: 6} 
+        program_type_mapping ={'0':'Exclusively Graduate',
+                       '1':'higher part-time',
+                       '2':'mixed part/full-time',
+                       '3':'medium full-time',
+                       '4':'higher full-time',
+                       '5':'higher part-time',
+                       '6':'medium full-time, inclusive, lower transfer-in',
+                       '7':'medium full-time, inclusive, higher transfer-in',
+                       '8':'medium full-time, selective, lower transfer-in',
+                       '9':'medium full-time , selective, higher transfer-in',
+                       '10':'full-time, inclusive, lower transfer-in',
+                       '11':'full-time, inclusive, higher transfer-in',
+                       '12':'full-time, selective, lower transfer-in',
+                       '13':'full-time, selective, higher transfer-in',
+                       '14':'full-time, more selective, lower transfer-in',
+                       '15':'full-time, more selective, higher transfer-in'
+                      }
+        duration_mapping ={'1':'4-year',
+                   '2':'2-year',
+                   '3':'less_than_2_years'
+                              }
                   
-            for result in results:
-                data = {
-                    'id': result['id'],
-                    'name': result['school.name'],
-                    'city': result['school.city'],
-                    'state': result['school.state'],
-                    'zip': result['school.zip'],
-                    'ownership': ownership_mapping.get(result['school.ownership'], 'Unknown'),
-                    'school_url': result['school.school_url'],
-                    'price_calculator': result['school.price_calculator_url'],
-                    'men_only': result['school.men_only'],
-                    'women_only': result['school.women_only'],
-                    'admission_rate': result['latest.admissions.admission_rate.overall'],
-                    'in_state_tution': result['latest.cost.tuition.in_state'],
-                    'out_of_state_tution': result['latest.cost.tuition.out_of_state'],
-                    'average_sat_scores': result['latest.admissions.sat_scores.average.overall'],
-                    'earnings_after_10_yrs_entry': result['latest.earnings.10_yrs_after_entry.median'],
-                    'research_output': research_mapping.get(result['school.carnegie_basic'], 7),
-                    'graduation_rate':result['latest.completion.completion_rate_4yr_150nt'],
-                    'faculty_quality':result['latest.student.demographics.student_faculty_ratio'],
-                    'international_outlook':result['latest.student.demographics.race_ethnicity.non_resident_alien'],
-                    'revenue_per_student':result['latest.school.tuition_revenue_per_fte'],
-                    'spending_per_student':result['latest.school.instructional_expenditure_per_fte'],
-                    'endowment':result['latest.school.endowment.end'],
-                    'full_time_retention_rate':result['latest.student.retention_rate.four_year.full_time_pooled'],
-                    'financial_aid_percent':result['latest.aid.pell_grant_rate']
-                    
+        program_category_mapping={'0':'Not classified',
+                      '1':'Associates Colleges: High Transfer-High Traditional',
+                      '3': 'Associates Colleges: High Transfer-High Nontraditional',
+                      '4': 'Associates Colleges: Mixed Transfer/Career & Technical-High Traditional',
+                      '5':'Associates Colleges: Mixed Transfer/Career & Technical-Mixed Traditional/Nontraditional',
+                      '6':'Associates Colleges: Mixed Transfer/Career & Technical-High Nontraditional',
+                      '7':'Associates Colleges: High Career & Technical-High Traditional',
+                      '8':'Associates Colleges: High Career & Technical-Mixed Traditional/Nontraditional',
+                      '9':'Associates Colleges: High Career & Technical-High Nontraditional',
+                      '10':'Health Professions',
+                      '11':'Technical Professions',
+                      '12':'Arts & Design',
+                      '13':'Other Fields',
+                      '14':'Baccalaureate/Associates Colleges: Associates Dominant',
+                      '15':'Doctoral Universities: Very High Research Activity',
+                      '16':'Doctoral Universities: High Research Activity',
+                      '17':'Doctoral/Professional Universities',
+                      '18':'Masters Colleges & Universities: Larger Programs',
+                      '19':'Masters Colleges & Universities: Medium Programs',
+                      '20':'Masters Colleges & Universities: Small Programs',
+                      '21':'Baccalaureate Colleges: Arts & Sciences Focus',
+                      '22':'Baccalaureate Colleges: Diverse Fields',
+                      '23':'Baccalaureate/Associates Colleges: Mixed Baccalaureate/Associates',
+                      '24':'Faith-Related Institutions',
+                      '25':'Medical Schools & Centers',
+                      '26':'Other Health Professions Schools',
+                      '27':'Research Institution',
+                      '28':'Engineering and Other Technology-Related Schools',
+                      '29':'Business & Management Schools',
+                      '30':'Arts, Music & Design Schools',
+                      '31':'Law Schools',
+                      '32':'Other Special Focus Institutions',
+                      '33':'Tribal Colleges'
+                                 }
+        data_list =[]
+        for result in results:
+            data = {
+                'id': result.get('id'),
+                'name': result.get('school.name'),
+                'city': result.get('school.city'),
+                'state': result.get('school.state'),
+                'zip': result.get('school.zip'),
+                'ownership': ownership_mapping.get(result.get('school.ownership'), 'Unknown'),
+                'school_url': result.get('school.school_url'),
+                'price_calculator': result.get('school.price_calculator_url'),
+                'men_only': result.get('school.men_only', False),
+                'women_only': result.get('school.women_only', False),
+                'admission_rate': result.get('latest.admissions.admission_rate.overall'),
+                'in_state_tuition': result.get('latest.cost.tuition.in_state'),
+                'out_of_state_tuition': result.get('latest.cost.tuition.out_of_state'),
+                'average_sat_scores': result.get('latest.admissions.sat_scores.average.overall'),
+                'earnings_after_10_yrs_entry': result.get('latest.earnings.10_yrs_after_entry.median'),
+                'research_output': research_mapping.get(result.get('school.carnegie_basic'), 7),
+                'graduation_rate': result.get('latest.completion.completion_rate_4yr_150nt'),
+                'faculty_quality': result.get('latest.student.demographics.student_faculty_ratio'),
+                'international_outlook': result.get('latest.student.demographics.race_ethnicity.non_resident_alien'),
+                'revenue_per_student': result.get('latest.school.tuition_revenue_per_fte'),
+                'spending_per_student': result.get('latest.school.instructional_expenditure_per_fte'),
+                'endowment': result.get('latest.school.endowment.end'),
+                'full_time_retention_rate': result.get('latest.student.retention_rate.four_year.full_time_pooled'),
+                'financial_aid_percent': result.get('latest.aid.pell_grant_rate'),
+                'program_category':program_category_mapping.get(result.get('school.carnegie_basic'),34),
+                'duration':duration_mapping.get(result.get('school.carnegie_basic'),'Unknown'),
+                'program_type':program_type_mapping.get(result.get('school.carnegie_undergrad'),'Unknown')
                 }
                 data_list.append(data)
             return data_list
