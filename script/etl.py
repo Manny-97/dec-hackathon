440718f8d6db3de2ab88e2b0f2c6406ff2678007
Choice Ugwuede <168081987+Choiceugwuede@users.noreply.github.com>
2025-03-26 13:01:44 +0100

    Update etl.py

--- script/etl.py
+++ script/etl.py
@@ -45,62 +45,63 @@ def read_data(page=0):
             print(f"Warning: Empty response received on page {page}")
             return None
         ownership_mapping = {1: "Public", 2: "Private Nonprofit", 3: "Private For-Profit"}
-        program_type_mapping ={'0':'Exclusively Graduate',
-                       '1':'higher part-time',
-                       '2':'mixed part/full-time',
-                       '3':'medium full-time',
-                       '4':'higher full-time',
-                       '5':'higher part-time',
-                       '6':'medium full-time, inclusive, lower transfer-in',
-                       '7':'medium full-time, inclusive, higher transfer-in',
-                       '8':'medium full-time, selective, lower transfer-in',
-                       '9':'medium full-time , selective, higher transfer-in',
-                       '10':'full-time, inclusive, lower transfer-in',
-                       '11':'full-time, inclusive, higher transfer-in',
-                       '12':'full-time, selective, lower transfer-in',
-                       '13':'full-time, selective, higher transfer-in',
-                       '14':'full-time, more selective, lower transfer-in',
-                       '15':'full-time, more selective, higher transfer-in'
+        research_mapping = {15: 1, 16: 2, 27: 3,17:4, 18: 5, 19: 6, 20: 7}
+        program_type_mapping ={0:'Exclusively Graduate',
+                       1:'higher part-time',
+                       2:'mixed part/full-time',
+                       3:'medium full-time',
+                       4:'higher full-time',
+                       5:'higher part-time',
+                       6:'medium full-time, inclusive, lower transfer-in',
+                       7:'medium full-time, inclusive, higher transfer-in',
+                       8:'medium full-time, selective, lower transfer-in',
+                       9:'medium full-time , selective, higher transfer-in',
+                       10:'full-time, inclusive, lower transfer-in',
+                       11:'full-time, inclusive, higher transfer-in',
+                       12:'full-time, selective, lower transfer-in',
+                       13:'full-time, selective, higher transfer-in',
+                       14:'full-time, more selective, lower transfer-in',
+                       15:'full-time, more selective, higher transfer-in'
                       }
-        duration_mapping ={'1':'4-year',
-                   '2':'2-year',
-                   '3':'less_than_2_years'
+        duration_mapping ={1:'4-year',
+                   2:'2-year',
+                   3:'less_than_2_years'
                               }
                   
-        program_category_mapping={'0':'Not classified',
-                      '1':'Associates Colleges: High Transfer-High Traditional',
-                      '3': 'Associates Colleges: High Transfer-High Nontraditional',
-                      '4': 'Associates Colleges: Mixed Transfer/Career & Technical-High Traditional',
-                      '5':'Associates Colleges: Mixed Transfer/Career & Technical-Mixed Traditional/Nontraditional',
-                      '6':'Associates Colleges: Mixed Transfer/Career & Technical-High Nontraditional',
-                      '7':'Associates Colleges: High Career & Technical-High Traditional',
-                      '8':'Associates Colleges: High Career & Technical-Mixed Traditional/Nontraditional',
-                      '9':'Associates Colleges: High Career & Technical-High Nontraditional',
-                      '10':'Health Professions',
-                      '11':'Technical Professions',
-                      '12':'Arts & Design',
-                      '13':'Other Fields',
-                      '14':'Baccalaureate/Associates Colleges: Associates Dominant',
-                      '15':'Doctoral Universities: Very High Research Activity',
-                      '16':'Doctoral Universities: High Research Activity',
-                      '17':'Doctoral/Professional Universities',
-                      '18':'Masters Colleges & Universities: Larger Programs',
-                      '19':'Masters Colleges & Universities: Medium Programs',
-                      '20':'Masters Colleges & Universities: Small Programs',
-                      '21':'Baccalaureate Colleges: Arts & Sciences Focus',
-                      '22':'Baccalaureate Colleges: Diverse Fields',
-                      '23':'Baccalaureate/Associates Colleges: Mixed Baccalaureate/Associates',
-                      '24':'Faith-Related Institutions',
-                      '25':'Medical Schools & Centers',
-                      '26':'Other Health Professions Schools',
-                      '27':'Research Institution',
-                      '28':'Engineering and Other Technology-Related Schools',
-                      '29':'Business & Management Schools',
-                      '30':'Arts, Music & Design Schools',
-                      '31':'Law Schools',
-                      '32':'Other Special Focus Institutions',
-                      '33':'Tribal Colleges'
-                                 }
+        program_category_mapping={0:'Not classified',
+                      1:'Associates Colleges: High Transfer-High Traditional',
+                      3: 'Associates Colleges: High Transfer-High Nontraditional',
+                      4: 'Associates Colleges: Mixed Transfer/Career & Technical-High Traditional',
+                      5:'Associates Colleges: Mixed Transfer/Career & Technical-Mixed Traditional/Nontraditional',
+                      6:'Associates Colleges: Mixed Transfer/Career & Technical-High Nontraditional',
+                      7:'Associates Colleges: High Career & Technical-High Traditional',
+                      8:'Associates Colleges: High Career & Technical-Mixed Traditional/Nontraditional',
+                      9:'Associates Colleges: High Career & Technical-High Nontraditional',
+                      10:'Health Professions',
+                      11:'Technical Professions',
+                      12:'Arts & Design',
+                      13:'Other Fields',
+                      14:'Baccalaureate/Associates Colleges: Associates Dominant',
+                      15:'Doctoral Universities: Very High Research Activity',
+                      16:'Doctoral Universities: High Research Activity',
+                      17:'Doctoral/Professional Universities',
+                      18:'Masters Colleges & Universities: Larger Programs',
+                      19:'Masters Colleges & Universities: Medium Programs',
+                      20:'Masters Colleges & Universities: Small Programs',
+                      21:'Baccalaureate Colleges: Arts & Sciences Focus',
+                      22:'Baccalaureate Colleges: Diverse Fields',
+                      23:'Baccalaureate/Associates Colleges: Mixed Baccalaureate/Associates',
+                      24:'Faith-Related Institutions',
+                      25:'Medical Schools & Centers',
+                      26:'Other Health Professions Schools',
+                      27:'Research Institution',
+                      28:'Engineering and Other Technology-Related Schools',
+                      29:'Business & Management Schools',
+                      30:'Arts, Music & Design Schools',
+                      31:'Law Schools',
+                      32:'Other Special Focus Institutions',
+                      33:'Tribal Colleges'
+                                }
         data_list =[]
         for result in results:
             data = {
@@ -129,7 +130,8 @@ def read_data(page=0):
                 'financial_aid_percent': result.get('latest.aid.pell_grant_rate'),
                 'program_category':program_category_mapping.get(result.get('school.carnegie_basic'),34),
                 'duration':duration_mapping.get(result.get('school.carnegie_basic'),'Unknown'),
-                'program_type':program_type_mapping.get(result.get('school.carnegie_undergrad'),'Unknown')
+                'program_type':program_type_mapping.get(result.get('school.carnegie_undergrad'),'Unknown'),
+                'research_output': research_mapping.get(result.get('school.carnegie_basic'), 8)
                 }
                 data_list.append(data)
             return data_list
